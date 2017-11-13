bc6c6f0b65fdb466128176496524cf295ce41073
Hazza1555 <UP808621@uni.ds.port.ac.uk>
2017-11-13 12:34:00 +0000

    addeed extras and the missing attuibutes of the object

--- ADPROC_Coursework/src/adproc_coursework/LongPipe.java
+++ ADPROC_Coursework/src/adproc_coursework/LongPipe.java
@@ -12,17 +12,19 @@ package adproc_coursework;
 public class LongPipe {
 
     private int lengthInMeters , diameterInInches , numOfColour , numOfPipes;
-    private boolean chemicalResist;
+    private boolean chemicalResist,insulation,reinforcement;
     
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    public LongPipe(int newLength,int newDiameter,int newNumColour, int newNumOfPipes,boolean newChemicalResist){
+    public LongPipe(int newLength,int newDiameter,int newNumColour, int newNumOfPipes,boolean newChemicalResist,boolean newInsulation,boolean newReinforcement){
         lengthInMeters = newLength;
         diameterInInches = newDiameter;
         numOfColour = newNumColour;
         numOfPipes = newNumOfPipes;
         chemicalResist = newChemicalResist;
+        insulation = newInsulation;
+        reinforcement = newReinforcement;
     }
     public double canculateVolume(int newLength,int newDiameter){
         //The volume of a cylinder = V=πr^2h
@@ -40,9 +42,27 @@ public class LongPipe {
     public double convertMeterToInch(int newLength){
         return newLength / 0.0254;
     }
-    public double cost(double volume,double price,double extraPercentage){
-        double cost = (volume*price)*extraPercentage;
-        return cost;
-    }
+    
+    public double canculateExtra(){
+        double extra = 1;
+        if (numOfColour == 1){
+            extra+=0.12;
+        }
+        if (numOfColour == 2){
+            extra+=0.16;
+        }
+         if (insulation == true){
+            extra+=0.13;
+        }
+        if(reinforcement == true){
+            extra +=0.17;
+        }
+        if (chemicalResist == true){
+            extra+=0.14;
+        }
+        return extra;
     
 }
+   
+}
+
