254bc582f85ffd4d6dc2d6cf56ee6e01dde25e1f
Hazza1555 <32413316+Hazza1555@users.noreply.github.com>
2017-11-13 12:17:36 +0000

    Add files via upload

---
+++ ADPROC_Coursework/src/adproc_coursework/PipeTypeI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adproc_coursework;
+
+/**
+ *
+ * @author up808621
+ */
+public class PipeTypeI extends LongPipe {
+    
+    public PipeTypeI(int newLength, int newDiameter, int newNumColour, int newNumOfPipes, boolean newChemicalResist, boolean newInsulation, boolean newReinforcement) {
+        super(newLength, newDiameter, newNumColour, newNumOfPipes, newChemicalResist, newInsulation, newReinforcement);
+    }
+    
+}
