472bafbee4064a7765a3586ea61f757b86e60921
Hazza1555 <UP808621@uni.ds.port.ac.uk>
2017-11-20 13:24:36 +0000

    added all changes to all subclasses

--- ADPROC_Coursework/src/adproc_coursework/PipeTypeII.java
+++ ADPROC_Coursework/src/adproc_coursework/PipeTypeII.java
@@ -11,11 +11,17 @@ package adproc_coursework;
  */
 public class PipeTypeII extends LongPipe {
     
-    public PipeTypeII(int newLength, int newDiameter, int newNumColour, int newNumOfPipes, boolean newChemicalResist, boolean newInsulation, boolean newReinforcement) {
-        super(newLength, newDiameter, newNumColour, newNumOfPipes, newChemicalResist, newInsulation, newReinforcement);
+    public PipeTypeII(int newGrade ,int newLength, int newDiameter, int newNumColour, int newNumOfPipes, boolean newChemicalResist, boolean newInsulation, boolean newReinforcement) {
+        super(newGrade ,newLength, newDiameter, newNumColour, newNumOfPipes, newChemicalResist, newInsulation, newReinforcement);
     }
-     public double cost(double volume,double price,double extra){
-       return (volume*1)*extra;
+    public double cost(double volume,double price,boolean ChemResist){
+      double costOfPipe = volume*price;
+      double extra = 1+0.12;
+      if (ChemResist == true){
+          extra += 0.14;
+      }
+      
+      return costOfPipe*extra;
 }
     
 }
